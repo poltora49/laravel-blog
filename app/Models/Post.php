dd89b3f1634f98bb260ea2b8d521d070fc1eb7f7
poltora49 <poltora49@gmail.com>
2023-03-10 15:18:41 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "description",
+        "thumbnail"
+    ];
+
+    public function comments(){
+        $this->hasMany(Comment::class)->orderBy("created_at");
+    }
+}
